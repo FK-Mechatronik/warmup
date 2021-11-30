98ce6eb6105184c8f0f8510cc705483c97a66936
FK <f-klueber@t-online.de>
2021-11-30 11:32:41 +0100

    Print "Hello World !"

--- source/HElloWorld.java
+++ source/HElloWorld.java
@@ -1,6 +1,7 @@
 public class HElloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !");
+        System.out.println("Hi Tobi");
         System.out.println("hellotest1234");
     }
 }
