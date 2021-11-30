7c2507e6f46d405f28704527789938403ed5858f
FK <f-klueber@t-online.de>
2021-11-30 11:39:04 +0100

    nice

--- source/HElloWorld.java
+++ source/HElloWorld.java
@@ -3,8 +3,8 @@ public class HElloWorld {
         System.out.println("Hello World !");
         System.out.println("Hi Tobi");
         System.out.println("hellotest1234");
-        System.out.println("TK zeile 6");
         System.out.println("nice");
         System.out.println("zeile5");
+
     }
 }
